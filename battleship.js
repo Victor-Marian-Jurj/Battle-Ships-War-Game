11af17549fabcd0ceff4c2e1113b94aca102a17b
victorjurj <victor.jurj@yahoo.com>
2023-02-09 22:52:03 +0200

    I maked a little and fun game called Battle Ship War.
    I have maked a fun little game that I used to play when I was younger called Battle Ship War. I used to play with friends and you needed a pen and a piece of papier.

---
+++ battleship.js
@@ -0,0 +1,107 @@
+let view = {
+  displayMessage: function (msg) {
+    document.getElementById("messageArea");
+    messageArea.innerHTML = msg;
+  },
+  displayHit: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "hit");
+  },
+  displayMiss: function (location) {
+    let cell = document.getElementById(location);
+    cell.setAttribute("class", "miss");
+  },
+};
+
+
+
+let model = {
+  boardSize: 7,
+  numShips: 3,
+  shipLength: 3,
+  shipsSunk: 0,
+
+  ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+  { locations: ["24", "34", "44"], hits: ["", "", ""] },
+  { locations: ["10", "11", "12"], hits: ["", "", ""] }],
+
+  fire: function (guess) {
+    for (let i = 0; i < this.numShips; i++) {
+      let ship = this.ships[i];
+      let locations = ship.locations;
+      let index = locations.indexOf(guess);
+      if (index >= 0) {
+        ship.hits[index] = "hit";
+        view.displayHit(guess);
+        view.displayMessage("HIT!");
+        if (this.isSunk(ship)) {
+          view.displayMessage("You sank my battleship!");
+          this.shipsSunk++;
+        }
+        return true;
+      }
+    }
+    view.displayMiss(guess);
+    view.displayMessage("You missed.");
+    return false;
+  },
+
+  isSunk: function (ship) {
+    for (let i = 0; i < this.shipLength; i++) {
+      if (ship.hits[i] !== "hit") {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+
+let controller = {
+  guesses: 0,
+
+  processGuess: function (guess) {
+    let location = parseGuess(guess);
+    if (location) {
+      this.guesses++;
+      let hit = model.fire(location);
+      if (hit && model.shipsSunk === model.numShips) {
+        view.displayMessage("You sank all my battleships, in " + this.guesses + " guesses");
+      }
+    }
+  }
+};
+function parseGuess(guess) {
+  var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+  if (guess === null || guess.length !== 2) {
+    alert("Oops, please enter a letter and a number on the board.");
+  } else {
+    let firstChar = guess.charAt(0);
+    let row = alphabet.indexOf(firstChar);
+    let column = guess.charAt(1);
+
+    if (isNaN(row) || isNaN(column)) {
+      alert("Oops, that isn't on the board.");
+    } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+      alert("Oops, that's off the board!");
+    } else {
+      return row + column;
+    }
+  }
+  return null;
+}
+
+function init() {
+  let fireButton = document.getElementById("fireButton");
+  fireButton.onclick = handleFireButton;
+}
+
+function handleFireButton() {
+  let guessInput = document.getElementById("guessInput");
+  let guess = guessInput.value;
+  controller.processGuess(guess);
+  guessInput.value = "";
+}
+
+window.onload = init;
+
